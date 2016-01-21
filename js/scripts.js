b4735caea4a2fb28bd2eda03dd9b0406827e59a6
jared beckler and nic netzel <jared.beckler@gmail.com>
2016-01-21 13:54:32 -0800

    add final styling and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 var puzzler = function(string){
-  return string = string.replace(/[aeiouAEIOU]/g,"-");
+  return string = string.replace(/[aeiou]/gi,"-");
 }
 
 $(document).ready(function() {
-  
 
+  $("form#puzzler").submit(function(event) {
+    var userInput = $("textarea#string").val();
+    var encodedText = puzzler(userInput);
 
+    $(".resultsText").show();
+    $("#encodedResults").text(encodedText);
+    $(".stringInput").hide();
+
+    event.preventDefault();
+  });
 });

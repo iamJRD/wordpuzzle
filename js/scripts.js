6baec6b2a4135ec544e9420ad091ad16007bf299
jared beckler and nic netzel <netzeln@gmail.com>
2016-01-21 13:32:10 -0800

    create test spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-
-
+var puzzler = function(string){
+  return string = string.replace(/[aeiouAEIOU]/g,"-");
+}
 
 $(document).ready(function() {
-
+  
 
 
 });
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("puzzler", function(){
+  it("will find all vowels and replace them with '-'", function(){
+    expect(puzzler("Boot")).to.equal("B--t");
+  });
+});
